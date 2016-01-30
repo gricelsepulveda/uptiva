ee3b65a069228842520677613ffde223fe9801ab
gricelsepulveda <gricel@gricelsepulveda.com>
2016-01-30 18:30:24 -0300

    primer avance

---
+++ js/script.js
@@ -0,0 +1,66 @@
+$(document).ready(function(){
+    /*ACTIVAR DROP*/
+    $(".btn_menu").click(function(){
+        if($(".menu").css("height")=='0px')
+            {
+                $(".menu").css({"height":'300px'});
+            }
+        else{
+                $(".menu").css({"height":'0px'});
+            }
+    });
+/*BANNER 1*/
+    var status = 2;
+
+    setInterval(function()
+    {
+        Slider(status);
+        if(status == 3) status = 1;
+        else status+= 1;
+    }, 5000);
+
+    function Slider(status)
+    {
+        if(status==1)
+        {
+            $("#slide_3").fadeOut("fast");
+            $("#slide_2").fadeOut("fast");
+            $("#slide_1").fadeIn("fast");
+        }
+
+        if(status==2)
+        {
+            $("#slide_1").fadeOut("fast");
+            $("#slide_3").fadeOut("fast");
+            $("#slide_2").fadeIn("fast");
+        }
+
+        if(status==3)
+        {
+            $("#slide_2").fadeOut("fast");
+            $("#slide_1").fadeOut("fast");
+            $("#slide_3").fadeIn("fast");
+        }
+    }
+/*NAVEGAR SECCIONES*/
+     $("#btn_inicio").click(function(){
+        $(".row_fluida").animate({"left":'0%'});
+    });
+     $("#btn_nosotros").click(function(){
+        $(".row_fluida").animate({"left":'-100%'});
+    });
+    $("#btn_servicios").click(function(){
+        $(".row_fluida").animate({"left":'-200%'});
+    });
+    $("#btn_portafolio").click(function(){
+        $(".row_fluida").animate({"left":'-300%'});
+    });
+    $("#btn_eventos").click(function(){
+        $(".row_fluida").animate({"left":'-400%'});
+    });
+    $("#btn_contacto").click(function(){
+        $(".row_fluida").animate({"left":'-500%'});
+    });
+});
+
+
