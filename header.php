59c541b4ed36ebf56e6fe4b855fbf809c94f1835
gricelsepulveda <gricel@gricelsepulveda.com>
2016-02-04 09:40:23 -0300

    servicios

---
+++ header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en" ng-app="app">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" href="<?php  echo base_url(); ?>css/uikit.min.css">
+	<link rel="stylesheet" href="<?php  echo base_url(); ?>css/jasny-bootstrap.min.css">
+	
+	<script type="text/javascript" src="<?php  echo base_url(); ?>js/jquery-2.1.4.min.js"></script>
+	<script type="text/javascript" src="<?php  echo base_url(); ?>js/angular.js"></script>
+	<script type="text/javascript" src="<?php  echo base_url(); ?>js/angular-route.js"></script>
+	<script type="text/javascript" src="<?php  echo base_url(); ?>js/login.js"></script>
+	<script type="text/javascript" src="<?php  echo base_url(); ?>js/uikit.min.js"></script>
+	<script type="text/javascript" src="<?php  echo base_url(); ?>js/bootbox.min.js"></script>
+	<script type="text/javascript" src="<?php  echo base_url(); ?>js/menu2.js"></script>
+	<script type="text/javascript" src="<?php  echo base_url(); ?>js/jasny-bootstrap.min.js"></script>
+
+
+	<link rel="stylesheet" href="<?php  echo base_url(); ?>css/bootstrap.min.css"><!--BOOTSTRAP CSS-->
+        <script src="<?php  echo base_url(); ?>js/bootstrap.min.js"></script><!--BOOTSTRAP JS-->
+        <script src="<?php  echo base_url(); ?>js/scripts.js"></script><!--SCRIPT PERSONALIZADO-->
+         <script src="<?php  echo base_url(); ?>js/script.js"></script><!--SCRIPT PERSONALIZADO-->
+        <script src="<?php  echo base_url(); ?>js/smooth-scroll.js"></script><!--SMOOTH SCROLL-->
+        <link rel="stylesheet" href="<?php  echo base_url(); ?>css/estilos.css"><!--ESTILOS-->
+        <link rel="stylesheet" href="<?php  echo base_url(); ?>fonts/glyphicons-halflings-regular.eot"><!--FUENTES BOOTSTRAP-->
+        <link rel="stylesheet" href="<?php  echo base_url(); ?>fonts/glyphicons-halflings-regular.svg">
+        <link rel="stylesheet" href="<?php  echo base_url(); ?>fonts/glyphicons-halflings-regular.ttf">
+        <link rel="stylesheet" href="<?php  echo base_url(); ?>fonts/glyphicons-halflings-regular.woff">
+        <link rel="stylesheet" href="<?php  echo base_url(); ?>fonts/glyphicons-halflings-regular.woff2">
+
+
+	
+	
+</head>
+<body>
